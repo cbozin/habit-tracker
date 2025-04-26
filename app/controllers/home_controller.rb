64480c750e55a25931db23acae7d1c4cd97e6708
Carolyn Bozin <carolynbozhinov@gmail.com>
2025-04-26 11:36:33 -0700

    added bootstrap (navbar)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+    before_action :authenticate_user!
     def index
+        render
+    end
+    def about
         render
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "home#index"
+
+  get "home/about"
 end

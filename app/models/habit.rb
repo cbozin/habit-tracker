6312a5b92f388cba3b8c7d439b91c5b37e0cc893
Carolyn Bozin <carolynbozhinov@gmail.com>
2025-04-29 11:16:02 -0700

    streak added

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -4,4 +4,14 @@ class Habit < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 500 }
+
+  def current_streak(user)
+    streak = 0
+    today = Date.today
+
+    while checkins.exists?(user: user, date: today - streak)
+      streak += 1
+    end
+    streak
+  end
 end

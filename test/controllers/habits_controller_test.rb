b393e27450c004d508c87930eef97367c220987a
Carolyn Bozin <carolynbozhinov@gmail.com>
2025-04-28 18:01:00 -0700

    added devise helper to habit tests

--- test/controllers/habits_controller_test.rb
+++ test/controllers/habits_controller_test.rb
@@ -1,6 +1,8 @@
 require "test_helper"
 
+
 class HabitsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @user = users(:one)
     sign_in @user

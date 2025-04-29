4602875cbca05e365d705329afcba3c8040e3ed4
Carolyn Bozin <carolynbozhinov@gmail.com>
2025-04-28 18:27:00 -0700

    fixing more checkin tests

--- test/controllers/checkins_controller_test.rb
+++ test/controllers/checkins_controller_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class CheckinsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @user = users(:one)
     sign_in @user
